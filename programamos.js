c99892a82fd3b73b6ee86203a5a349d09b39879c
juandavidyepes <judavidy@hotmail.com>
2021-03-18 19:48:03 -0500

    jd commit

--- programamos.js
+++ programamos.js
@@ -5,46 +5,47 @@ const server = express();
 server.use(express.json());
 
 let personas = [
-    { id: 1, nombre: 'Pepe', email: 'pepe@nada.com' },
-    { id: 2, nombre: 'Hugo', email: 'hugo@nada.com' }, 
-    { id: 3, nombre: 'Juan', email: 'juan@nada.com' }
-]
+  { id: 1, nombre: 'Pepe', email: 'pepe@nada.com' },
+  { id: 2, nombre: 'Hugo', email: 'hugo@nada.com' },
+  { id: 3, nombre: 'Luis', email: 'juan@nada.com' },
+];
 
 validarIdPersona = (req, res, next) => {
-    const id = parseInt(req.params.id);
-    let resultado = personas.find(persona => persona.id === id);
-    if (resultado) {
-        req.encontrado = resultado;
-        next();
-    } else {
-        res.status(404).send({error: 'Persona no encontrada'})
-    }
-}
+  const id = parseInt(req.params.id);
+  let resultado = personas.find((persona) => persona.id === id);
+  if (resultado) {
+    req.encontrado = resultado;
+    next();
+  } else {
+    res.status(404).send({ error: 'Persona no encontrada' });
+  }
+};
 
 validarNombrePersona = (req, res, next) => {
-    const nombre = req.query.nombre.toLowerCase();
-    let resultado = personas.find(persona => persona.nombre.toLowerCase() === nombre);
-    if (resultado) {
-        req.encontrado = resultado;
-        next();
-    } else {
-        res.status(404).send({error: 'Persona no encontrada'})
-    }
-}
+  const nombre = req.query.nombre.toLowerCase();
+  let resultado = personas.find(
+    (persona) => persona.nombre.toLowerCase() === nombre
+  );
+  if (resultado) {
+    req.encontrado = resultado;
+    next();
+  } else {
+    res.status(404).send({ error: 'Persona no encontrada' });
+  }
+};
 
 server.get('/personas/:id', validarIdPersona, (req, res) => {
-    let objeto = {a: 1, b: 3}
-    console.log(JSON.stringify(objeto));
-    objeto.c = 4;
-    console.log(JSON.stringify(objeto));
-    res.status(200).send(req.encontrado);
-})
+  let objeto = { a: 1, b: 3 };
+  console.log(JSON.stringify(objeto));
+  objeto.c = 4;
+  console.log(JSON.stringify(objeto));
+  res.status(200).send(req.encontrado);
+});
 
 server.get('/personas', validarNombrePersona, (req, res) => {
-    res.status(200).send(req.encontrado);
-})
+  res.status(200).send(req.encontrado);
+});
 
 server.listen(process.env.SERVER_PORT, () => {
-    console.log('Todo listo papá...')
-})
-    
+  console.log('Todo listo papá...');
+});
